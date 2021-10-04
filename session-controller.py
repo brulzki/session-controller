c13107e9086a3609f5f243dc931632a9d9de2aef
Bruce Schultz <brulzki@gmail.com>
2021-10-04 12:15:14 +1000

    Adds a retry handler for dbus method calls

--- session-controller.py
+++ session-controller.py
@@ -50,17 +50,34 @@ class AlsaseqMonitor(Pub):
                 self.post(Event('seq_client_exit', clientid=event.get_data()['addr.client']))
 
 
+# jack_retry decorator
+# org.freedesktop.DBus.Error.ServiceUnknown occurs if the Jack DBus service
+# has exited since the interface was initialised; reconnect and retry
+def jack_retry(func):
+    def inner(obj, *args, **kwargs):
+        try:
+            return func(obj, *args, **kwargs)
+        except dbus.exceptions.DBusException as e:
+            print('jack_retry failed:', e.get_dbus_name())
+            obj._reconnect()
+            return func(obj, *args, **kwargs)
+    return inner
+
+
 class JackMonitor(Pub):
     def __init__(self, bus=None):
         super().__init__()
         self.bus = bus or dbus.SessionBus()
-        self.jack = self.bus.get_object('org.jackaudio.service', '/org/jackaudio/Controller')
-        self.jackctl = dbus.Interface(self.jack, dbus_interface='org.jackaudio.JackControl')
-        self.jackcfg = dbus.Interface(self.jack, dbus_interface='org.jackaudio.Configure')
+        self._reconnect()
         self.bus.add_signal_receiver(self.on_started, dbus_interface='org.jackaudio.JackControl',
                                      signal_name='ServerStarted')
         self.bus.add_signal_receiver(self.on_stopped, dbus_interface='org.jackaudio.JackControl',
                                      signal_name='ServerStopped')
+
+    def _reconnect(self):
+        self.jack = self.bus.get_object('org.jackaudio.service', '/org/jackaudio/Controller')
+        self.jackctl = dbus.Interface(self.jack, dbus_interface='org.jackaudio.JackControl')
+        self.jackcfg = dbus.Interface(self.jack, dbus_interface='org.jackaudio.Configure')
 
     def on_started(self, *args, **kwargs):
         self.post(Event('jack_started'))
@@ -68,27 +85,34 @@ class JackMonitor(Pub):
     def on_stopped(self, *args, **kwargs):
         self.post(Event('jack_stopped'))
 
+    @jack_retry
     def is_started(self):
         return self.jackctl.IsStarted()
 
+    @jack_retry
     def start(self):
         return self.jackctl.StartServer()
 
+    @jack_retry
     def stop(self):
         return self.jackctl.StopServer()
 
+    @jack_retry
     def get_engine_parameter(self, parameter):
         return self.jackcfg.GetParameterValue(['engine', parameter])[2]
 
+    @jack_retry
     def set_engine_parameter(self, parameter, value):
         if value is None:
             return bool(self.jackcfg.ResetParameterValue(['engine', parameter]))
         else:
             return bool(self.jackcfg.SetParameterValue(['engine', parameter], value))
 
+    @jack_retry
     def get_driver_parameter(self, parameter):
         return self.jackcfg.GetParameterValue(['driver', parameter])[2]
 
+    @jack_retry
     def set_driver_parameter(self, parameter, value):
         if value is None:
             return bool(self.jackcfg.ResetParameterValue(['driver', parameter]))
