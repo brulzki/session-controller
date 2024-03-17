1acd6969123387f932760668845138935998e845
Bruce Schultz <brulzki@gmail.com>
2024-03-17 10:34:00 +1000

    Switch to use pluginbase for loading plugins
    Drop straight.plugin; it was not possible to load plugins from outside
    the python path with that module.

--- sessionlib/plugin.py
+++ sessionlib/plugin.py
@@ -1,13 +1,24 @@
-try:
-    from straight.plugin import load
-except ModuleNotFoundError:
-    import sys
-    sys.path.append('straight.plugin')
-    from straight.plugin import load
+import os
+from pluginbase import PluginBase
 
-from . import Plugin
+import sessionlib
 
 class Manager():
-    def __init__(self, path, controller):
-       plugin_classes = load('plugins', subclasses=Plugin)
-       self.plugins = plugin_classes.produce(controller)
+    def __init__(self, path, *args, **kwargs):
+        namespace = 'sessionlib.plugins'
+        subclass = sessionlib.Plugin
+
+        if path.startswith('~'):
+            path = os.path.expanduser(path)
+
+        plugin_base = PluginBase(package=namespace)
+        self._source = plugin_base.make_plugin_source(searchpath=[path])
+
+        self.plugins = []
+        for plugin_name in self._source.list_plugins():
+            module = self._source.load_plugin(plugin_name)
+            for name in dir(module):
+                attr = getattr(module, name)
+                if isinstance(attr, type) and \
+                   issubclass(attr, subclass) and attr is not subclass:
+                        self.plugins.append(attr(*args, **kwargs))
